57afc51a76ff1c7dd75d1af615c0d7203c284dcf
Lars Weisheit <lars.weisheit@evopro-ag.de>
2022-01-21 09:35:16 +0100

    Add initialization for internal and external uvicorn calls

--- main.py
+++ main.py
@@ -174,10 +174,11 @@ def api_get_windowstates(log: Optional[bool] = None):
 
 
 if __name__ == '__main__':
+    # initialize and run uvicorn when called as a module
     load_config()
-    # login()
-    # get_data()
-    # logout()
     print(room_data)
-
     uvicorn.run(api, port=8000, host="127.0.0.1")
+else:
+    # initialize when called by externally run uvicorn
+    load_config()
+    print(room_data)

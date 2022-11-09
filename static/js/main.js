8da6bee09e292e2a43573962cb96d2f0354fcee0
Lars Weisheit <lars.weisheit@evopro-ag.de>
2022-11-09 08:25:26 +0100

    Add name setting of rooms by API call

--- static/js/main.js
+++ static/js/main.js
@@ -6,14 +6,15 @@ function getdata() {
     $.get(ccu3api_url, function(data){
         console.log(data);
         for (var i = 0; i < data.rooms.length; i++) {
-            setstate(i, data.rooms[i].state);
+            setstate(i, data.rooms[i].state, data.rooms[i].name);
         }
     });
 }
 
-function setstate(room_number, state) {
+function setstate(room_number, state, name) {
     var room_id = "#btn_room_" + room_number;
-    //console.log("Setting room " + room_number + " to " + state);
+    console.log("Setting room " + room_number + " (" + name + ") to " + state);
+    $(room_id).text(name);
     if (state) {
         $(room_id).removeClass('btn-danger');
         $(room_id).addClass('btn-success');
